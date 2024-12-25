b3d0c761107a014913a7647f38cdbdf6cc3fff2c
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-12-25 12:40:35 -0500

    finish implementing CIDs

--- atyolo/src/dasl/cid.rs
+++ atyolo/src/dasl/cid.rs
@@ -1,6 +1,10 @@
 //! https://dasl.ing/cid.html
 
-use super::errors::{DaslError::ParseError, Result};
+use super::{
+    errors::{DaslError::ParseError, Result},
+    DaslError,
+};
+use core::str;
 use std::borrow::Cow;
 
 #[derive(Clone, Copy, Eq, PartialEq, Debug)]
@@ -14,6 +18,15 @@ pub enum Codec {
 pub struct CID<'a> {
     pub codec: Codec,
     pub hash: Cow<'a, [u8]>,
+}
+
+impl Codec {
+    fn to_hex(self) -> u8 {
+        match self {
+            Codec::Raw => 0x55,
+            Codec::DCBOR42 => 0x71,
+        }
+    }
 }
 
 /// Decode LEB128, starting at `off`. Returns the result, and modifies `off`.
@@ -35,7 +48,7 @@ fn dec_leb128(s: &[u8], off: &mut usize) -> Option<u64> {
     }
 }
 
-macro_rules! fail {
+macro_rules! parse_err {
     ($msg:expr) => {
         return Err(ParseError($msg))
     };
@@ -43,23 +56,23 @@ macro_rules! fail {
 
 fn parse_binary_<'a>(s: &'a [u8]) -> Result<CID<'a>> {
     if s.len() < 3 {
-        fail!("CID too short");
+        parse_err!("CID too short");
     };
 
     let version = s[0];
     if version != 1 {
-        fail!("Expected version to be 1")
+        parse_err!("Expected version to be 1")
     }
     let codec = s[1];
 
     let codec = match codec {
         0x55 => Codec::Raw,
         0x71 => Codec::DCBOR42,
-        _ => fail!("Invalid codec"),
+        _ => parse_err!("Invalid codec"),
     };
     let hash_type = s[2];
     if hash_type != 0x12 {
-        fail!("Expected hash type to be SHA256");
+        parse_err!("Expected hash type to be SHA256");
     }
 
     let mut offset: usize = 3;
@@ -67,10 +80,10 @@ fn parse_binary_<'a>(s: &'a [u8]) -> Result<CID<'a>> {
         dec_leb128(s, &mut offset).ok_or_else(|| ParseError("Invalid LEB128 length"))? as usize;
 
     if len != 32 {
-        fail!("Expected length to be 32 (for SHA256)")
+        parse_err!("Expected length to be 32 (for SHA256)")
     }
     if offset + len != s.len() {
-        fail!("CID has the wrong length")
+        parse_err!("CID has the wrong length")
     }
 
     Ok(CID {
@@ -79,12 +92,66 @@ fn parse_binary_<'a>(s: &'a [u8]) -> Result<CID<'a>> {
     })
 }
 
-/// Parse a binary CID
-pub fn parse_binary<'a>(s: &'a [u8]) -> Result<CID<'a>> {
-    if s.is_empty() || s[0] != 0 {
-        fail!("CID is too short")
+const SIZE: usize = 32 + 4;
+
+fn encode_binary(cid: &'_ CID, res: &mut [u8; SIZE]) {
+    res[0] = 1;
+    res[1] = cid.codec.to_hex();
+    res[2] = 0x12; // hash
+    debug_assert_eq!(32, cid.hash.len());
+    res[3] = 32; // len=32 in LEB128
+    res[4..].copy_from_slice(&cid.hash);
+}
+
+impl<'a> CID<'a> {
+    pub fn to_owned(self) -> CID<'static> {
+        let owned: Vec<u8> = self.hash.into_owned();
+        CID {
+            codec: self.codec,
+            hash: Cow::Owned(owned),
+        }
     }
-    parse_binary_(&s[1..])
+    pub fn encode_to_binary(&'_ self, res: &mut Vec<u8>) {
+        res.push(0);
+        res.reserve(SIZE);
+        let mut slice = [0u8; SIZE];
+        encode_binary(self, &mut slice);
+        res.extend_from_slice(&slice);
+    }
+
+    pub fn encode_to_string(&'a self) -> String {
+        let mut res = String::new();
+        res.push('b');
+        let mut slice = [0u8; SIZE];
+        encode_binary(self, &mut slice);
+        let b32_res = base32::encode(base32::Alphabet::Rfc4648Lower { padding: false }, &slice);
+        res += &b32_res;
+        res
+    }
+
+    /// Parse a binary CID
+    pub fn parse_binary(s: &'a [u8]) -> Result<CID<'a>> {
+        if s.is_empty() || s[0] != 0 {
+            parse_err!("CID is too short")
+        }
+        parse_binary_(&s[1..])
+    }
+
+    pub fn parse_str(s: &str) -> Result<CID<'static>> {
+        let s = s.as_bytes();
+        if s[0] != b'b' {
+            parse_err!("Expected string CID to start with 'b'")
+        }
+
+        // first char is ascii, can just skip 1 byte
+        let s_rest: &str = str::from_utf8(&s[1..]).unwrap();
+
+        let bin: Vec<u8> =
+            base32::decode(base32::Alphabet::Rfc4648Lower { padding: false }, s_rest)
+                .ok_or_else(|| DaslError::ParseError("invalid base32"))?;
+        let cid = parse_binary_(&bin)?;
+        Ok(cid.to_owned())
+    }
 }
 
 #[cfg(test)]
@@ -106,6 +173,21 @@ mod tests {
         }
     }
 
+    // TODO: proptest for CID => binary => CID
+    // TODO: proptest for CID => string => CID
+
+    fn ref_cid() -> CID<'static> {
+        CID {
+            codec: Codec::Raw,
+            hash: Cow::from(&[
+                1, 1, 1, 1, 1, 1, 1, 1, /* */
+                2, 2, 2, 2, 2, 2, 2, 2, /* */
+                3, 3, 3, 3, 3, 3, 3, 3, /* */
+                4, 4, 4, 4, 4, 4, 4, 4,
+            ]),
+        }
+    }
+
     #[test]
     fn test_decode_binary() {
         let fake_cid: &[u8] = &[
@@ -116,18 +198,18 @@ mod tests {
             4, 4, 4, 4, 4, 4, 4, 4,
         ];
 
-        let cid = parse_binary(fake_cid).unwrap();
-        assert_eq!(
-            CID {
-                codec: Codec::Raw,
-                hash: Cow::from(&[
-                    1, 1, 1, 1, 1, 1, 1, 1, /* */
-                    2, 2, 2, 2, 2, 2, 2, 2, /* */
-                    3, 3, 3, 3, 3, 3, 3, 3, /* */
-                    4, 4, 4, 4, 4, 4, 4, 4,
-                ]),
-            },
-            cid
-        );
+        let cid = CID::parse_binary(fake_cid).unwrap();
+        assert_eq!(ref_cid(), cid);
+
+        let mut encoded = vec![];
+        cid.encode_to_binary(&mut encoded);
+        assert_eq!(fake_cid, &encoded);
+    }
+
+    #[test]
+    fn test_decode_str() {
+        let cid_str = "bafkreiabaeaqcaibaeaqeaqcaibaeaqcambqgaydambqgbaeaqcaibaeaq";
+        let parsed = CID::parse_str(cid_str).unwrap();
+        assert_eq!(ref_cid(), parsed)
     }
 }
