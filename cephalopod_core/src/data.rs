c1a45913b09f3321ec5f2ce88ee430c9a3b2bbc7
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-01-05 23:39:28 -0500

    core: more helpers

--- cephalopod_core/src/data.rs
+++ cephalopod_core/src/data.rs
@@ -57,6 +57,11 @@ macro_rules! as_case {
 }
 
 impl<'a> Value<'a> {
+    #[inline(always)]
+    pub fn is_null(&self) -> bool {
+        self == &Value::Null
+    }
+
     as_case!(as_map, Value::Map, &'a [(&'a str, Value<'a>)]);
     as_case!(as_array, Value::Array, &'a [Value<'a>]);
     as_case!(as_cid, Value::CID, &'a CID);
@@ -65,6 +70,15 @@ impl<'a> Value<'a> {
     as_case!(as_positive, deref, Value::Positive, u64);
     as_case!(as_negagive, deref, Value::Negative, u64);
     as_case!(as_bool, deref, Value::Bool, bool);
+
+    pub fn as_i64(&self) -> Option<i64> {
+        match self {
+            Value::Positive(i) if *i <= i64::MAX as u64 => Some(*i as i64),
+            Value::Negative(i) if *i == ((i64::MAX as u64) + 1) => Some(i64::MIN),
+            Value::Negative(i) if *i <= (i64::MAX as u64) => Some(-(*i as i64)),
+            _ => None,
+        }
+    }
 
     /// Read a blob from this value.
     pub fn as_blob(&self) -> Option<Blob<'a>> {
@@ -134,6 +148,93 @@ impl<'a> Value<'a> {
     pub fn from_json(alloc: &'a Bump, j: &serde_json::Value) -> Result<Value<'a>> {
         dcbor42::from_json_rec(alloc, j, 0)
     }
+
+    #[inline(always)]
+    pub fn from_i64(n: i64) -> Self {
+        if n >= 0 {
+            Value::Positive(n as u64)
+        } else {
+            Value::Negative((-(n as i128)) as u64)
+        }
+    }
+
+    #[inline(always)]
+    pub fn from_text(alloc: &'a Bump, str: &'_ str) -> Result<Value<'a>> {
+        let str = utils::alloc_str(alloc, str)?;
+        Ok(Value::Text(str))
+    }
+
+    #[inline(always)]
+    pub fn from_bytes(alloc: &'a Bump, bytes: &'_ [u8]) -> Result<Value<'a>> {
+        let str = utils::alloc_slice_copy(alloc, bytes)?;
+        Ok(Value::Bytes(str))
+    }
+
+    pub fn from_array<T>(alloc: &'a Bump, items: &[T]) -> Result<Value<'a>>
+    where
+        T: Encodable<'a>,
+    {
+        if items.is_empty() {
+            return Ok(Value::Array(&[]));
+        }
+
+        // encode args
+        let mut items_vec = Vec::with_capacity(items.len());
+        for x in items.iter() {
+            items_vec.push(x.encode(alloc)?)
+        }
+
+        let items = utils::alloc_slice(alloc, items_vec.len(), items_vec[0].clone())?;
+        items.clone_from_slice(&items_vec);
+        Ok(Value::Array(items))
+    }
+}
+
+/// Builder for maps
+pub struct MapBuilder<'a> {
+    alloc: &'a Bump,
+    items: Vec<(&'a str, Value<'a>)>,
+}
+
+impl<'a> MapBuilder<'a> {
+    pub fn new(alloc: &'a Bump) -> Self {
+        MapBuilder {
+            alloc,
+            items: vec![],
+        }
+    }
+
+    /// Add a key/value pair
+    pub fn add<T>(&mut self, k: &'_ str, v: &T) -> Result<&mut Self>
+    where
+        T: Encodable<'a>,
+    {
+        let k = utils::alloc_str(self.alloc, k)?;
+        let v = v.encode(self.alloc)?;
+        self.items.push((k, v));
+        Ok(self)
+    }
+
+    pub fn add_opt<T>(&mut self, k: &'_ str, v: &Option<T>) -> Result<&mut Self>
+    where
+        T: Encodable<'a>,
+    {
+        if let Some(v) = v {
+            self.add(k, v)?;
+        }
+        Ok(self)
+    }
+
+    pub fn into_value(self) -> Result<Value<'a>> {
+        let items = if self.items.is_empty() {
+            &mut []
+        } else {
+            let slice = utils::alloc_slice(self.alloc, self.items.len(), self.items[0].clone())?;
+            slice.clone_from_slice(&self.items);
+            slice
+        };
+        Ok(Value::Map(items))
+    }
 }
 
 impl<'a> Blob<'a> {
@@ -159,3 +260,16 @@ pub trait Encodable<'a>: Sized + 'a {
     /// Decode a value.
     fn decode(alloc: &'a Bump, v: &Value) -> crate::Result<Self>;
 }
+
+// FIXME: need to deep copy??
+/*
+impl<'a> Encodable<'a> for Value<'a> {
+    fn encode<'res>(&'_ self, alloc: &'res Bump) -> crate::Result<crate::Value<'res>> {
+        Ok(self.clone())
+    }
+
+    fn decode(_alloc: &'a Bump, v: &Value) -> crate::Result<Self> {
+        Ok(v.clone())
+    }
+}
+*/
